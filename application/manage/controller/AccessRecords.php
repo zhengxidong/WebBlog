a3bc18ca0c8605d9ff78f1ee522ab1f45de5126e
zhengxidong <541410829@qq.com>
2018-06-19 15:30:12 +0800

    #增加文章访问记录

--- application/manage/controller/AccessRecords.php
+++ application/manage/controller/AccessRecords.php
@@ -3,12 +3,20 @@ namespace app\manage\controller;
 use app\manage\controller\Base;
 use think\Controller;
 use think\Session;
+use think\Db;
 use app\manage\model\AccessRecords as AccessRecordsModel;
 class AccessRecords extends Base
 {
     public function index()
     {
-      $accessRecords = AccessRecordsModel::select();
+      //$accessRecords = AccessRecordsModel::select();
+
+      $accessRecords = Db::table('bg_access_records')
+      ->alias('ar')
+      ->join('bg_article a','a.id = ar.article_id')
+      ->order('ar.id','desc')
+      ->select();
+
       $this->assign('accessRecordsList',$accessRecords);
       return $this->view->fetch();
     }
--- application/manage/controller/Base.php
+++ application/manage/controller/Base.php
@@ -8,6 +8,18 @@ class Base extends Controller
   protected function _initialize()
   {
     parent::_initialize();//继承父类中的初始化操作
+
+    $expire = 3 * 60 * 60;   //session过期时间
+    //$expire = 60;   //session过期时间
+    if(time() - Session::get('session_start_time') > $expire)
+    {
+      //销毁所有信息
+      Session::clear();
+      $this->redirect('admin/index');
+      //exit;
+    }
+    //var_dump(Session::get('admin_id'));
+    //var_dump(Session::get('session_start_time'));
     define('ADMIN_ID',Session::get('admin_id'));
   }
   //判断用户是否登录,放在后台的入口：index/index
@@ -21,6 +33,8 @@ class Base extends Controller
   //防止用户重复登录 user/login
   protected function alreadyLogin()
   {
+
+
     if(!empty(ADMIN_ID))
     {
       $this->error('用户已经登录,请勿重复登录','manage/index');
