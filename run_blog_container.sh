024c1f3aced2d8791508096668508cbfb9a8aefa
zhengxidong <541410829@qq.com>
2019-02-24 14:31:31 +0800

    添加构建镜像文件

--- run_blog_container.sh
+++ run_blog_container.sh
@@ -2,8 +2,8 @@
 
 docker run -d -p 80:80 --name blog \
 	-v ./application:/usr/local/nginx/html/www.itellyou.site/application \
-	-v ./extend:/usr/local/nginx/html/www.itellyou.site/extend \ 
-	-v ./public:/usr/local/nginx/html/www.itellyou.site/public \ 
-	-v ./thinkphp:/usr/local/nginx/html/www.itellyou.site/thinkphp \ 
-	-v ./vendor:/usr/local/nginx/html/www.itellyou.site/vendor \ 
+	-v ./extend:/usr/local/nginx/html/www.itellyou.site/extend \
+	-v ./public:/usr/local/nginx/html/www.itellyou.site/public \
+	-v ./thinkphp:/usr/local/nginx/html/www.itellyou.site/thinkphp \
+	-v ./vendor:/usr/local/nginx/html/www.itellyou.site/vendor \
 	blog
